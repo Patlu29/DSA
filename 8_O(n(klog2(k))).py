782f592cab3f57f6c98ac7b236a15d06b2c93e96
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2024-12-19 12:03:32 +0530

    create big O notation O(n(klog2(k))) for sorting a list of strings

---
+++ 8_O(n(klog2(k))).py
@@ -0,0 +1,21 @@
+def result(strings):
+    for i,string in enumerate(strings): # this takes n complexity
+        digits = 0
+        
+        for char in string :
+            if char in [str(i) for i in range(0,10)]: # this is a constant bcoz it iterates 0-9(10)
+                digits += 1
+                print(digits)
+                
+        if digits >= len(string) / 2:
+            strings[i] = sorted(strings[i])
+            print(strings)
+            
+    return strings
+    
+print(result(["HELLO"]))
+
+"""
+the time complexity of this code is O(n(k log2(k)))
+space complexity is O(n * k)    
+"""
